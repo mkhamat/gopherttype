752408e6b26f0114d5cff1124a369dc40a8d43a7
mkhamat <streetmc1337@gmail.com>
2026-09-09 11:21:10 +0300

    refactor(home): consolidate settings into the screen model

--- internal/app/home/model.go
+++ internal/app/home/model.go
@@ -1,11 +1,35 @@
 package home
 
 import (
+	"time"
+
 	tea "charm.land/bubbletea/v2"
 
 	"gopherttype/internal/app/ui"
 	"gopherttype/internal/engine"
 )
+
+var (
+	wordPresets     = [...]int{10, 25, 50, 100}
+	durationPresets = [...]time.Duration{15 * time.Second, 30 * time.Second, 60 * time.Second, 120 * time.Second}
+)
+
+type settings struct {
+	mode          engine.Mode
+	wordIndex     int
+	durationIndex int
+}
+
+func defaultSettings() settings {
+	return settings{mode: engine.ModeTime, durationIndex: 1}
+}
+
+func (s settings) config() engine.Config {
+	if s.mode == engine.ModeTime {
+		return engine.Config{Mode: s.mode, Duration: durationPresets[s.durationIndex]}
+	}
+	return engine.Config{Mode: s.mode, WordCount: wordPresets[s.wordIndex]}
+}
 
 type StartMsg struct{ Config engine.Config }
 
